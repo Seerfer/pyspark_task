1d9f373759ad8675781c1ce8cfda40f11be3dc5e
Patryk Ostrowski <patrykost1998@gmail.com>
2022-03-15 14:41:17 +0100

    added filtter function for dataframe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
+from typing import Union, TypedDict
 
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame 
+import pyspark.sql.functions as F
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 def setSparkSession():  
@@ -29,3 +31,23 @@ def create_scheams():
     ])
 
     return schema1, schema2
+
+def read_file(path: str, spark: SparkSession, schema: Union[StructType, None] = None) -> DataFrame:
+    if schema is not None:
+        return spark.read.schema(schema).option("header", "true").csv(path)
+    else: 
+       return spark.read.option("header", "true").csv(path)
+
+
+def filter_df_equal(df: DataFrame, column_value: TypedDict) -> DataFrame:
+    for col, values in column_value.items():
+        df=df.filter(F.col(f"{col}").isin(values))
+    return df
+
+
+schema1, schema2 = create_scheams()
+spark = setSparkSession()
+df1 = read_file("dataset_one.csv", spark)
+filter_values ={"country": "Netherlands"}
+df1 = filter_df_equal(df1, filter_values)
+df1.show()
